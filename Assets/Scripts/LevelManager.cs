e1e5abfbf4d005c90f47590b09578f5dbc7e852f
MatthewYPYK <matt.yongpiyakul@gmail.com>
2024-03-06 01:52:57 +0700

    first commit tiles + camera pan

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] tilePrefabs;
+
+    [SerializeField]
+    public CameraMovement cameraMovement;
+
+    public Dictionary<Point, TileScript> Tiles { get; set; }
+
+    public float TileSize
+    {
+        get { return tilePrefabs[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        createLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void createLevel()
+    {
+        Tiles = new Dictionary<Point, TileScript>();
+
+        string[] mapData = ReadLevelText();
+        char[] rowData = null;
+
+        Vector3 maxTile = Vector3.zero;
+        
+        Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
+
+        for (int y = 0; y < mapData.Length; y++) {
+            rowData = mapData[y].ToCharArray();
+
+            for (int x = 0; x < rowData.Length; x++) {
+                PlaceTile(rowData[x].ToString(), x, y, worldStart);
+            }
+        }
+        maxTile = Tiles[new Point(rowData.Length - 1, mapData.Length - 1)].transform.position;
+
+        cameraMovement.SetLimits(new Vector3(maxTile.x + TileSize, maxTile.y - TileSize));
+    }
+
+    private void PlaceTile(string tileType, int x, int y, Vector3 worldStart)
+    {
+        int tileIndex = int.Parse(tileType);
+        TileScript newTile = Instantiate(tilePrefabs[tileIndex]).GetComponent<TileScript>();
+
+        newTile.Setup(new Point(x, y), new Vector3(worldStart.x + (TileSize * x), worldStart.y - (TileSize * y), 0));
+
+        Tiles.Add(new Point(x, y), newTile);
+    }
+
+    private string[] ReadLevelText()
+    {
+        TextAsset bindData = Resources.Load("Level") as TextAsset;
+
+        string data = bindData.text.Replace(Environment.NewLine, string.Empty);
+
+        return data.Split('-');
+    }
+}
