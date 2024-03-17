b73da5f7c1ec237c562fbc0354a79d3405b8d41b
ThanachatAim <Thanachat.aim@gmail.com>
2024-03-17 20:35:12 +0700

    7.2 : Spawn effect

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,5 +7,23 @@ public class Enemy : MonoBehaviour
    public void Spawn()
    {
         transform.position = LevelManager.Instance.GreenPortal.transform.position;
+
+        StartCoroutine(Scale(new Vector3(0.1f,0.1f),new Vector3(1,1)));
    }
+
+    public IEnumerator Scale(Vector3 from, Vector3 to)
+    {
+        float progress = 0;
+
+        while (progress <=1)
+        {
+            transform.localScale = Vector3.Lerp(from,to,progress);
+
+            progress += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localScale = to;
+    }
 }
