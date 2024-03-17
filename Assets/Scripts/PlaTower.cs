8a874b7c721e1c086675bb21e012b525db76ebbc
MinitrueSecretary <wipugeng@hotmail.com>
2024-03-18 02:06:46 +0700

    Add PlaTower file

---
+++ Assets/Scripts/PlaTower.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaTower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Select()
+    {
+        Debug.Log("Tower selected");
+        spriteRenderer.enabled = !spriteRenderer.enabled;
+    }
+}
